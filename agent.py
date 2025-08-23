e5d40199b8817ea3c66bd1533642508010083590
Charbel Khazen <chkhazen@c2r3s2.42beirut.com>
2025-08-23 11:40:10 +0300

    changed file names and removed unnecessary files

---
+++ agent.py
@@ -0,0 +1,19 @@
+class Agent:
+    def __init__(self, asset, liab):
+        self.asset = asset
+        self.liab = liab
+        self.bal = asset + liab
+    def __add__(self, other):
+        x = self.bal
+        y = other.bal
+        return (x + y)
+    def __repr__(self):
+        return (f'Agent({self.asset!r}, {self.liab!r})')
+    def __str__(self):
+        return (f"Assets: {self.asset}, Liabilities: {self.liab}")
+
+bob = Agent(100, 0)
+ray = Agent(0 , 100)
+print(repr(ray))
+print(ray)
+print(ray + bob)
